e0b56d96c55fb74561e7d80605fff6ac26ed72d0
Prashant Dubey <pdubey1924@gmail.com>
2025-02-27 17:21:45 +0530

    up

--- app/app/page.jsx
+++ app/app/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-
+import Link from "next/link";
 import { useState, useEffect, useCallback } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import axios from 'axios';
